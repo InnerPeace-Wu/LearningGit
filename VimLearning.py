c62f9a2e30e0ff494b9ab49def7ead3a0f226a08
InnerPeace-Wu <innerpeace.wu@outlook.com>
2016-11-28 21:36:04 +0800

    update undo method

--- VimLearning.py
+++ VimLearning.py
@@ -1,1 +1,8 @@
 #Vim basic learning, happy hacking
+1. u && :undo  #undo
+2. CTRL_R && :redo #redo
+   :undolist  #to check the undo list
+3. g- :back to earlier
+   g+ :foward to newer
+   :earlier :before a specific time
+   :later   :after a specific time
